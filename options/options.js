aa98405c1dc302de0ed3a0bcfd69bab2d81535ea
Crowdbotics <team@crowdbotics.com>
2024-10-11 18:35:12 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "calculator-49650",
+  url: "https://calculator-49650.botics.co",
+  api: "https://calculator-49650.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
